da2d1893b167ed93885b331d23050f3706ce9eb3
Ali Sal <ali@moveaheadmedia.co.uk>
2022-12-08 16:05:48 +0700

    Added no cache

--- mam-quform-utms.php
+++ mam-quform-utms.php
@@ -67,7 +67,7 @@ add_action('quform_pre_display', function (Quform_Form $form) {
                 var data = {
                     'action': 'mam_get_session'
                 };
-                jQuery.post('<?php echo admin_url( 'admin-ajax.php' ); ?>', data, function(response) {
+                jQuery.post('<?php echo admin_url( 'admin-ajax.php' ); ?>?nocache=' + time(), data, function(response) {
                     var data = JSON.parse(response);
                     <?php
                     foreach ($utm_source as $element_name){
